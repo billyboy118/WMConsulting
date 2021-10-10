a14420c8365462e62ae63d8eed9afe2bb61fcc6a
billyboy118 <bill_matley@hotmail.com>
2021-10-10 13:11:15 +0100

    In a good position for Heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,30 +2,37 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
+//= require jquery.slick
+
 import 'bootstrap'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import 'bootstrap/js/src/alert'  
-import 'bootstrap/js/src/button'  
-import 'bootstrap/js/src/carousel'  
-import 'bootstrap/js/src/collapse'  
-import 'bootstrap/js/src/dropdown'  
-import 'bootstrap/js/src/modal'  
-import 'bootstrap/js/src/popover'  
-import 'bootstrap/js/src/scrollspy'  
-import 'bootstrap/js/src/tab'  
-import 'bootstrap/js/src/toast'  
-import 'bootstrap/js/src/tooltip'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+
+
+// import 'bootstrap/js/src/alert'  
+// import 'bootstrap/js/src/button'  
+// import 'bootstrap/js/src/carousel'  
+// import 'bootstrap/js/src/collapse'  
+// import 'bootstrap/js/src/dropdown'  
+// import 'bootstrap/js/src/modal'  
+// import 'bootstrap/js/src/popover'  
+// import 'bootstrap/js/src/scrollspy'  
+// import 'bootstrap/js/src/tab'  
+// import 'bootstrap/js/src/toast'  
+// import 'bootstrap/js/src/tooltip'
 
 
 
 
-
-(function ($) {
-  'use strict';
 
   /* ========================================================================= */
   /*	Page Preloader
@@ -41,191 +48,192 @@ import 'bootstrap/js/src/tooltip'
     });
   });
 
+
   
-  //Hero Slider
-  $('.hero-slider').slick({
-    autoplay: true,
-    infinite: true,
-    arrows: true,
-    prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
-    nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
-    dots: false,
-    autoplaySpeed: 7000,
-    pauseOnFocus: false,
-    pauseOnHover: false
-  });
-  $('.hero-slider').slickAnimation();
+  // //Hero Slider
+  // $('.hero-slider').slick({
+  //   autoplay: true,
+  //   infinite: true,
+  //   arrows: true,
+  //   prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
+  //   nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
+  //   dots: false,
+  //   autoplaySpeed: 7000,
+  //   pauseOnFocus: false,
+  //   pauseOnHover: false
+  // });
+  // $('.hero-slider').slickAnimation();
 
 
   /* ========================================================================= */
   /*	Header Scroll Background Change
   /* ========================================================================= */
 
-  $(window).scroll(function () {
+
+
+
+  window.onscroll = function () {
+    
     var scroll = $(window).scrollTop();
     //console.log(scroll);
     if (scroll > 200) {
-      //console.log('a');
+      console.log('a');
       $('.navigation').addClass('sticky-header');
     } else {
-      //console.log('a');
+      console.log('a');
       $('.navigation').removeClass('sticky-header');
     }
-  });
+  };
 
   /* ========================================================================= */
   /*	Portfolio Filtering Hook
   /* =========================================================================  */
 
     // filter
-    setTimeout(function(){
-      var containerEl = document.querySelector('.filtr-container');
-      var filterizd;
-      if (containerEl) {
-        filterizd = $('.filtr-container').filterizr({});
-      }
-    }, 500);
+//     setTimeout(function(){
+//       var containerEl = document.querySelector('.filtr-container');
+//       var filterizd;
+//       if (containerEl) {
+//         filterizd = $('.filtr-container').filterizr({});
+//       }
+//     }, 500);
 
-  /* ========================================================================= */
-  /*	Testimonial Carousel
-  /* =========================================================================  */
+//   /* ========================================================================= */
+//   /*	Testimonial Carousel
+//   /* =========================================================================  */
 
-  //Init the slider
-  $('.testimonial-slider').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000
-  });
-
-
-  /* ========================================================================= */
-  /*	Clients Slider Carousel
-  /* =========================================================================  */
-
-  //Init the slider
-  $('.clients-logo-slider').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 5,
-    slidesToScroll: 1
-  });
+//   //Init the slider
+//   $('.testimonial-slider').slick({
+//     infinite: true,
+//     arrows: false,
+//     autoplay: true,
+//     autoplaySpeed: 2000
+//   });
 
 
+//   /* ========================================================================= */
+//   /*	Clients Slider Carousel
+//   /* =========================================================================  */
+
+//   //Init the slider
+//   $('.clients-logo-slider').slick({
+//     infinite: true,
+//     arrows: false,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     slidesToShow: 5,
+//     slidesToScroll: 1
+//   });
 
 
-  /* ========================================================================= */
-  /*	Company Slider Carousel
-  /* =========================================================================  */
-  $('.company-gallery').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 5,
-    slidesToScroll: 1
-  });
 
 
-  /* ========================================================================= */
-  /*   Contact Form Validating
-  /* ========================================================================= */
+//   /* ========================================================================= */
+//   /*	Company Slider Carousel
+//   /* =========================================================================  */
+//   $('.company-gallery').slick({
+//     infinite: true,
+//     arrows: false,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     slidesToShow: 5,
+//     slidesToScroll: 1
+//   });
 
-  $('#contact-form').validate({
-      rules: {
-        name: {
-          required: true,
-          minlength: 4
-        },
-        email: {
-          required: true,
-          email: true
-        },
-        subject: {
-          required: false
-        },
-        message: {
-          required: true
-        }
-      },
-      messages: {
-        user_name: {
-          required: 'Come on, you have a name don\'t you?',
-          minlength: 'Your name must consist of at least 2 characters'
-        },
-        email: {
-          required: 'Please put your email address'
-        },
-        message: {
-          required: 'Put some messages here?',
-          minlength: 'Your name must consist of at least 2 characters'
-        }
-      },
-      submitHandler: function (form) {
-        $(form).ajaxSubmit({
-          type: 'POST',
-          data: $(form).serialize(),
-          url: 'sendmail.php',
-          success: function () {
-            $('#contact-form #success').fadeIn();
-          },
-          error: function () {
-            $('#contact-form #error').fadeIn();
-          }
-        });
-      }
-    }
 
-  );
+//   /* ========================================================================= */
+//   /*   Contact Form Validating
+//   /* ========================================================================= */
 
-  /* ========================================================================= */
-  /*	On scroll fade/bounce effect
-  /* ========================================================================= */
-  var scroll = new SmoothScroll('a[href*="#"]');
+//   $('#contact-form').validate({
+//       rules: {
+//         name: {
+//           required: true,
+//           minlength: 4
+//         },
+//         email: {
+//           required: true,
+//           email: true
+//         },
+//         subject: {
+//           required: false
+//         },
+//         message: {
+//           required: true
+//         }
+//       },
+//       messages: {
+//         user_name: {
+//           required: 'Come on, you have a name don\'t you?',
+//           minlength: 'Your name must consist of at least 2 characters'
+//         },
+//         email: {
+//           required: 'Please put your email address'
+//         },
+//         message: {
+//           required: 'Put some messages here?',
+//           minlength: 'Your name must consist of at least 2 characters'
+//         }
+//       },
+//       submitHandler: function (form) {
+//         $(form).ajaxSubmit({
+//           type: 'POST',
+//           data: $(form).serialize(),
+//           url: 'sendmail.php',
+//           success: function () {
+//             $('#contact-form #success').fadeIn();
+//           },
+//           error: function () {
+//             $('#contact-form #error').fadeIn();
+//           }
+//         });
+//       }
+//     }
 
-  // -----------------------------
-  //  Count Up
-  // -----------------------------
-  function counter() {
-    if ($('.counter').length !== 0) {
-      var oTop = $('.counter').offset().top - window.innerHeight;
-    }
-    if ($(window).scrollTop() > oTop) {
-      $('.counter').each(function () {
-        var $this = $(this),
-          countTo = $this.attr('data-count');
-        $({
-          countNum: $this.text()
-        }).animate({
-          countNum: countTo
-        }, {
-          duration: 1000,
-          easing: 'swing',
-          step: function () {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function () {
-            $this.text(this.countNum);
-          }
-        });
-      });
-    }
-  }
-  // -----------------------------
-  //  On Scroll
-  // -----------------------------
-  $(window).scroll(function () {
-    counter();
-  });
-})(jQuery);
+//   );
 
+//   /* ========================================================================= */
+//   /*	On scroll fade/bounce effect
+//   /* ========================================================================= */
+//   var scroll = new SmoothScroll('a[href*="#"]');
+
+//   // -----------------------------
+//   //  Count Up
+//   // -----------------------------
+//   function counter() {
+//     if ($('.counter').length !== 0) {
+//       var oTop = $('.counter').offset().top - window.innerHeight;
+//     }
+//     if ($(window).scrollTop() > oTop) {
+//       $('.counter').each(function () {
+//         var $this = $(this),
+//           countTo = $this.attr('data-count');
+//         $({
+//           countNum: $this.text()
+//         }).animate({
+//           countNum: countTo
+//         }, {
+//           duration: 1000,
+//           easing: 'swing',
+//           step: function () {
+//             $this.text(Math.floor(this.countNum));
+//           },
+//           complete: function () {
+//             $this.text(this.countNum);
+//           }
+//         });
+//       });
+//     }
+//   }
+//   // -----------------------------
+//   //  On Scroll
+//   // -----------------------------
+//   $(window).scroll(function () {
+//     counter();
+//   });
 
 
 
 
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+
